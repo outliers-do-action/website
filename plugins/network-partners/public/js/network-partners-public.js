af7a75e035fca5b7cae828d6b194627c99835762
Gerhard <potgieterg@gmail.com>
2020-06-15 10:57:19 +0200

    Open network partner links in new window

--- plugins/network-partners/public/js/network-partners-public.js
+++ plugins/network-partners/public/js/network-partners-public.js
@@ -1,32 +1,13 @@
 (function( $ ) {
 	'use strict';
-
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
-
+	$( document ).ready( function() {
+		// Open links in new windows.
+		$( '.ptam-block-post-grid-image a' ).click( function() {
+			console.log('Triggered');
+			if ( '#' !== this.href[this.href.length -1] ) {
+				window.open( this.href );
+			}
+			return false;
+		} );
+	} );
 })( jQuery );
